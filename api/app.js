990047003180b8f5461908a3d1c8ba1fdd1f5ec5
Saad Aziz <saad0914@gmail.com>
2022-08-26 23:36:32 -0700

    mmm, cookies

--- api/app.js
+++ api/app.js
@@ -51,7 +51,6 @@ app.use(session({
 }));
 if (app.get('env') === 'production') {
   app.set('trust proxy', 1) // trust first proxy
-  // sess.cookie.secure = true // serve secure cookies
 }
 
 // set up data sources
